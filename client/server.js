26c1da8fda650271b65644f06093aade10b485bf
Satish R <satish.r.venkat@gmail.com>
2022-06-18 23:49:03 +0530

    changed server.js

--- client/server.js
+++ client/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.static('./dist/client'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/client/'}),
+    res.sendFile('index.html', {root: 'dist/client/index.html/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
