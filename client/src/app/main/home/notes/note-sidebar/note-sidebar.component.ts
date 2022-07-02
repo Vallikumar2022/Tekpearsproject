c6d9c8294b3ff8336ce9148149a8f3b0ba290556
Satish R <satish.r.venkat@gmail.com>
2022-07-02 20:26:56 +0530

    removed unwanted files

--- client/src/app/main/home/notes/note-sidebar/note-sidebar.component.ts
+++ client/src/app/main/home/notes/note-sidebar/note-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from 'express';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-note-sidebar',
